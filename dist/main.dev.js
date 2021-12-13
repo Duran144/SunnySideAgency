b827f34be84a895be2cef3cd42c8eac9e1aab806
Duran <duran@gmail.com>
2021-12-13 16:33:20 +0200

    *

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,22 +1,7 @@
 "use strict";
 
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("dropdown").classList.toggle("show");
-} // Close the dropdown menu if the user clicks outside of it
-
-
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
+var btn = document.querySelector('.mobile-navbar');
+var modal = document.querySelector('.links');
+btn.addEventListener('click', function () {
+  modal.classList.toggle('open');
+});
