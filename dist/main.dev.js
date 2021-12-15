50dec2003cbed864b9942ddc3ca0f88827da1acd
Duran <duran@gmail.com>
2021-12-15 16:41:57 +0200

    Fixed navbar

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,7 +1,1 @@
 "use strict";
-
-var btn = document.querySelector('.mobile-navbar');
-var modal = document.querySelector('.links');
-btn.addEventListener('click', function () {
-  modal.classList.toggle('open');
-});
--- main.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('.mobile-navbar');
-const modal = document.querySelector('.links');
-
-btn.addEventListener('click', () => {
-  modal.classList.toggle('open');
-});
